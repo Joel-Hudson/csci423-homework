30886ea9ad5313af36ce18a4a2d06c57eb1694c9
Joel-Hudson <70236962+Joel-Hudson@users.noreply.github.com>
2020-09-01 17:59:16 -0400

    Add files via upload

---
+++ uniquify.rkt
@@ -0,0 +1,24 @@
+#lang racket
+(require "utilities.rkt")
+(require "interp-R1.rkt")
+(require racket/trace)
+
+(trace-define (uniquify-exp symtab)
+  (lambda (e)
+    (match e
+      [(Var x) (Var (dict-ref symtab x))]
+      [(Int n) (Int n)]
+      [(Let x e body) (define new-x (gensym x))
+                      (define new-env (dict-set symtab x new-x))
+                      (define new-e ((uniquify-exp new-env) e))
+                      (define new-body ((uniquify-exp new-env) body))
+                      
+         (Let new-x new-e new-body)]
+      [(Prim op es)
+       (Prim op (for/list ([e es]) ((uniquify-exp symtab) e)))])))
+
+(define (uniquify p)
+(match p
+[(Program info e)
+(Program info ((uniquify-exp '()) e))]))
+                      
